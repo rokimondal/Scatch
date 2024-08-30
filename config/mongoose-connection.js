47df576dd1e761aa8812ea83156885e22f56c688
Roki Mondal <rokimondal974833@gmail.com>
2024-08-31 02:08:03 +0530

    creating dynamic mongodb url create owner functionality in development filled

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
+const config = require('config');
+
+const dbgr = require('debug')("development : mongoose");
+
 mongoose
-    .connect('mongodb://127.0.0.1:27017/scatch')
+    .connect(`${config.get("MONGODB_URI")}/scatch`)
     .then(function () {
-        console.log("connected")
+        dbgr("connected");
+        // console.log("connected");
     })
     .catch(function (err) {
-        console.error(err);
+        dbgr(err);
     })
 module.exports = mongoose.connection;
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,7 +1,29 @@
 const express = require('express')
 const router = express.Router();
+const ownerModel = require('../models/owner-model')
 
-router.get("/", function(req, res){
+
+if (process.env.NODE_ENV) {
+    router.post("/create", async function (req, res) {
+        let owners = await ownerModel.find();
+        if (owners.length > 0) {
+            return res
+                .status(501)
+                .send("You can't create a new owner");
+        }
+        let { fullname, email, password } = req.body;
+        let createOwner = await ownerModel.create({
+            fullname,
+            email,
+            password,
+        })
+        res.status(203).send(createOwner);
+    })
+}
+
+
+
+router.get("/", function (req, res) {
     res.send("It's working");
 })
 
